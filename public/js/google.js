2af517378c93aa0973694a9a8623df05e8cb2cae
manharr <142604277+manharr@users.noreply.github.com>
2024-10-07 12:13:04 +0530

    Update google.js

--- public/js/google.js
+++ public/js/google.js
@@ -4,12 +4,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/fireba
 import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider,sendPasswordResetEmail,fetchSignInMethodsForEmail  } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvk4BSP8zqonNGmYRUsqFbXuUTu8Kw0MM",
-    authDomain: "login-form-c55dc.firebaseapp.com",
-    projectId: "login-form-c55dc",
-    storageBucket: "login-form-c55dc.appspot.com",
+    apiKey: "API",
+    authDomain: "login-form-c51dc.firebaseapp.com",
+    projectId: "login-form-c51dc",
+    storageBucket: "login-form-c51dc.appspot.com",
     messagingSenderId: "725345110293",
-    appId: "1:725345110293:web:8590de145d691aefed011c"
+    appId: "1:7293:web:8590011c"
 };
 
 // Initialize Firebase
