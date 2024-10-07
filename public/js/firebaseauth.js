7278c13c959f60e3f7b8e3091d1a5a87e61341cc
manharr <142604277+manharr@users.noreply.github.com>
2024-10-07 12:13:18 +0530

    Update firebaseauth.js

--- public/js/firebaseauth.js
+++ public/js/firebaseauth.js
@@ -8,14 +8,13 @@ import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDvk4BSP8zqonNGmYRUsqFbXuUTu8Kw0MM",
-    authDomain: "login-form-c55dc.firebaseapp.com",
-    projectId: "login-form-c55dc",
-    storageBucket: "login-form-c55dc.appspot.com",
+    apiKey: "API",
+    authDomain: "login-form-c51dc.firebaseapp.com",
+    projectId: "login-form-c51dc",
+    storageBucket: "login-form-c51dc.appspot.com",
     messagingSenderId: "725345110293",
-    appId: "1:725345110293:web:8590de145d691aefed011c"
+    appId: "1:7293:web:8590011c"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
