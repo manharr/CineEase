8dc900cc99909f30ac16811fb620fc487edeaa8d
manharr <142604277+manharr@users.noreply.github.com>
2024-10-07 12:15:00 +0530

    Update edit.js

--- public/js/edit.js
+++ public/js/edit.js
@@ -3,12 +3,12 @@ import { getAuth, onAuthStateChanged, updateProfile } from "https://www.gstatic.
 import { getFirestore, doc, updateDoc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDvk4BSP8zqonNGmYRUsqFbXuUTu8Kw0MM",
-    authDomain: "login-form-c55dc.firebaseapp.com",
-    projectId: "login-form-c55dc",
-    storageBucket: "login-form-c55dc.appspot.com",
+    apiKey: "API",
+    authDomain: "login-form-c51dc.firebaseapp.com",
+    projectId: "login-form-c51dc",
+    storageBucket: "login-form-c51dc.appspot.com",
     messagingSenderId: "725345110293",
-    appId: "1:725345110293:web:8590de145d691aefed011c"
+    appId: "1:7293:web:8590011c"
 };
 
 const app = initializeApp(firebaseConfig);
