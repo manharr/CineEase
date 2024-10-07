965901826fdfbd85d20944be57be4e5459ae4fc6
manharr <142604277+manharr@users.noreply.github.com>
2024-10-07 12:15:31 +0530

    Update book.js

--- public/js/book.js
+++ public/js/book.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/fireba
         import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
         const firebaseConfig = {
-            apiKey: "AIzaSyDvk4BSP8zqonNGmYRUsqFbXuUTu8Kw0MM",
-            authDomain: "login-form-c55dc.firebaseapp.com",
-            projectId: "login-form-c55dc",
-            storageBucket: "login-form-c55dc.appspot.com",
-            messagingSenderId: "725345110293",
-            appId: "1:725345110293:web:8590de145d691aefed011c"
-        };
+    apiKey: "API",
+    authDomain: "login-form-c51dc.firebaseapp.com",
+    projectId: "login-form-c51dc",
+    storageBucket: "login-form-c51dc.appspot.com",
+    messagingSenderId: "725345110293",
+    appId: "1:7293:web:8590011c"
+};
 
         const app = initializeApp(firebaseConfig);
         const auth = getAuth(app);
