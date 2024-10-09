1c36087d0796b37734c66e4a49b40ac494483165
manharr <142604277+manharr@users.noreply.github.com>
2024-10-09 18:32:20 +0530

    Update Contact.php

--- app/models/Contact.php
+++ app/models/Contact.php
@@ -5,7 +5,6 @@ class Contact {
         $name = mysqli_real_escape_string($con, $name);
         $email = mysqli_real_escape_string($con, $email);
         $message = mysqli_real_escape_string($con, $message);
-
         $query = "INSERT INTO contactus (uname, mail, message) VALUES ('$name', '$email', '$message')";
         return mysqli_query($con, $query);
     }
